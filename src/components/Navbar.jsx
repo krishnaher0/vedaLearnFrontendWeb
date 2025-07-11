5eed715a2ded08cf4c4e9f48e925289205b4965e
krishnaher0 <krishna613460@gmail.com>
2025-07-11 23:59:58 +0545

    Refactor Navbar component to use AuthContext for user information and handle logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,16 @@
-// Header.js
-import React from 'react';
-import { FaSearch, FaBell, FaUserCircle } from 'react-icons/fa';
-import { useState,useEffect } from 'react';
+import React,{ useContext } from "react";
+import { AuthContext } from "../auth/AuthProvider";
+import { FaSearch, FaBell, FaUserCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-   const [admin, setAdmin] = useState({ name: "", email: "" });
+  const navigate=useNavigate();
+  const { user, logout } = useContext(AuthContext);
+  const handleLogout=()=>{
+    logout();
+    navigate("/login")
+  }
 
- useEffect(() => {
-    try {
-      const storedAdmin = JSON.parse(localStorage.getItem("user"));
-      console.log("Stored admin from localStorage:", storedAdmin);
-      if (storedAdmin && storedAdmin.email) {
-        setAdmin(storedAdmin);
-      }
-    } catch (error) {
-      console.error("Failed to parse admin from localStorage", error);
-    }
-  }, []);
   return (
     <header className="flex justify-between items-center bg-white p-4 shadow-sm rounded-lg mb-6">
       <div className="flex items-center border border-gray-300 rounded-md py-2 px-3">
@@ -30,13 +24,19 @@ function Navbar() {
       <div className="flex items-center">
         <FaBell className="text-gray-500 text-xl mr-5 cursor-pointer hover:text-gray-700" />
         <div className="text-right mr-3">
-          <span className="block font-semibold text-gray-800">{admin.name || "Admin"}</span>
-          <span className="block text-sm text-gray-600">{admin.email}</span>
+          <span className="block font-semibold text-gray-800">{user?.name || "Admin"}</span>
+          <span className="block text-sm text-gray-600">{user?.email || ""}</span>
         </div>
-        <FaUserCircle className="text-blue-500 text-3xl cursor-pointer" />
+        <FaUserCircle className="text-blue-500 text-3xl cursor-pointer mr-3" />
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition"
+        >
+          Logout
+        </button>
       </div>
     </header>
   );
 }
 
-export default Navbar
+export default Navbar;
